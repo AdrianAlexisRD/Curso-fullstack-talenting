37211e80e2f9fcb074d997bebb75efc043058aad
Alex09121995 <158055482+Alex09121995@users.noreply.github.com>
2025-05-15 11:56:59 -0400

    Se codifico la tarea 4 para dividir por funciones y agregar el localStorge

--- tarea-4/script.js
+++ tarea-4/script.js
@@ -1,21 +1,14 @@
 const input = document.querySelector('#entrada')
 const button = document.getElementById('add')
-const conteinerList = document.getElementById('conteinerList')
+const ul = document.getElementById('conteinerList')
 const catidadTareasGuardadas = document.getElementById('cantidad-tarea')
 const tareasRealizadas = document.getElementById('tareas-realisadas')
 const tareasNoRealizadas =document.getElementById('tareas-no-realizadas')
 
 let data = []
 
-const mainFunction = (input)=>{
-button.addEventListener('click', () => {
-  const tarea = input.value.trim();
-  if (tarea === "" || tarea.length < 4) {
-    alert('coloque una tarea válida')
-    return
-  }
 
-  
+function crearLi (tarea){
   const li = document.createElement('li')
   li.classList.add('tarea')
   li.innerHTML = `
@@ -23,50 +16,75 @@ button.addEventListener('click', () => {
     <div class="conteiner-check-delete">
     <input class="check" type="checkbox">
     <div class="borrar">Delete</div></div>`
-  conteinerList.appendChild(li)
+  ul.appendChild(li)
   input.value = ""
   
   actualizarConteo()
+  almacenarTarea(tarea)
+  subrrayarTareaYborrar(li)
+} 
+
+const createElement = (input)=>{
+button.addEventListener('click', () => {
+  const tarea = input.value.trim();
+  if (tarea === "" || tarea.length < 4) {
+    alert('coloque una tarea válida')
+    return
+  }
+crearLi(tarea)
+})};
+createElement(input)
+
+// Marcar como checked, subrrayar y borrar tareas
+const subrrayarTareaYborrar = (li)=>{
   const checkbox = li.querySelector('.check')
   const borrar = li.querySelector('.borrar')
   const parrafo = li.querySelector('p')
 
-  const t = li.children[0].innerText
-  data.push(t)
-  console.log(t)
-  console.log(data)
-  localStorage.setItem('llave', data )
-
-console.log(checkbox)
 // subrrayar o verificar checkbox
-  checkbox.addEventListener('click', () => {
+    checkbox.addEventListener('click', () => {
+    console.log(li)
     parrafo.classList.toggle('subrrayar')
     actualizarConteo()
   });
 
-// borramos tarea
+  // borramos tarea
   borrar.addEventListener('click', () => {
     console.log(li)
     li.remove()
     actualizarConteo()
   });
-});
 }
-mainFunction(input)
-const desplegarTareas= ()=>{
-const tareasGuardadas =localStorage.getItem('llave')
-console.log(tareasGuardadas)
-
-}
-desplegarTareas()
 
 // Función para actualizar contadores
 const actualizarConteo=()=>{
-  const tareas = conteinerList.querySelectorAll('li')
-  const completadas = conteinerList.querySelectorAll('.check:checked').length
-  const pendientes = tareas.length - completadas
-  catidadTareasGuardadas.innerText = tareas.length
+  const tareas = ul.querySelectorAll('li').length
+  const completadas = ul.querySelectorAll('.check:checked').length
+  const pendientes = tareas - completadas
+  catidadTareasGuardadas.innerText = tareas
   tareasRealizadas.innerText = completadas
   tareasNoRealizadas.innerText = pendientes
 }
 actualizarConteo()
+
+const almacenarTarea=(tarea)=>{
+  const t = {tareasGuardadas : tarea}
+  data.push(t)
+  console.log(t)
+  console.log(data)
+  localStorage.setItem('llave', JSON.stringify(data))
+}
+
+const desplegarTareas= ()=>{
+const tareasGuardadas =JSON.parse(localStorage.getItem('llave'))
+imprimirTareasGuardadas(tareasGuardadas)
+console.log(tareasGuardadas)}
+
+const imprimirTareasGuardadas=(tareas)=>{
+  for (let tarea of tareas){
+    console.log(tarea)
+    crearLi(tarea.tareasGuardadas)
+  }
+}
+desplegarTareas()
+localStorage.clear()
